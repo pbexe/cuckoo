a059d4dfc9637fe67c941b63433108ecba8eea89
Zac <allenzj@cardiff.ac.uk>
2017-10-21 17:44:22 +0100

    Latest files for differenceCheck.py

--- differenceCheck.py
+++ differenceCheck.py
@@ -2,54 +2,51 @@
 # flight time
 # time difference
 import requests
-import geonames
+import urllib
 
 # x and y is location name googlemaps
-differenceCheck(x,y)
-String(x)
-String(y)
-# concatinate string with google API for Lat/Long request
-responseX = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+x, safe=' '))
-responseY = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+y, safe=' '))
+def differenceCheck(x,y):
+	x = urllib.parse.quote(x, safe='')
+	y = urllib.parse.quote(y, safe='')
 
-# Parse for Lat/Long
-respX_json_payload = responseX.json()
-respY_json_payload = responseY.json()
+	# concatinate string with google API for Lat/Long request
+	responseX = requests.get('https://maps.googleapis.com/maps/api/geocode/'+x)
+	responseY = requests.get('https://maps.googleapis.com/maps/api/geocode/'+y)
 
-# print Lat/Long in dictionaries
-x = (respX_json_payload['results'][0]['geometry']['location'])
-y = (respY_json_payload['results'][0]['geometry']['location'])
+	# Parse for Lat/Long
+	respX_json_payload = responseX.json()
+	respY_json_payload = responseY.json()
 
-# Split Lat/Long from dictionary
-xLat = x['lat'] 
-xLong = x['lng']
+	# print Lat/Long in dictionaries
+	x = (respX_json_payload['results'][0]['geometry']['location'])
+	y = (respY_json_payload['results'][0]['geometry']['location'])
 
-yLat = y['lat']
-yLong = y['lng']
+	# Split Lat/Long from dictionary
+	xLat = x['lat'] 
+	xLong = x['lng']
 
-# Get timezone
+	yLat = y['lat']
+	yLong = y['lng']
 
-geonames_client = geonames.GeonamesClient('demo')
-geonames_resultX = geonames_client.find_timezone({xlat, xlong})
-geonames_resultY = geonames_client.find_timezone({ylat, ylong})
+	# Get timezone
 
-# Test prints
-# print(geonames_resultX['timezoneId'])
-# print(geonames_resultY['timezoneId'])
+	geonames_client = geonames.GeonamesClient('demo')
+	geonames_resultX = geonames_client.find_timezone({xlat, xlong})
+	geonames_resultY = geonames_client.find_timezone({ylat, ylong})
 
-# final variables available to other functions
-# xFinal = geonames_resultX['timezoneId']
-# yFinal = geonames_resultY['timezoneId']
+	# Test prints
+	# print(geonames_resultX['timezoneId'])
+	# print(geonames_resultY['timezoneId'])
 
-# ALT METHOD (Doesn't use geonames module)
-urlX = "http://api.geonames.org/timezoneJSON?formatted=true&lat={}&lng={}&username=demo".format(xLat,xLong)
-urlY = "http://api.geonames.org/timezoneJSON?formatted=true&lat={}&lng={}&username=demo".format(yLat,yLong)
+	# final variables available to other functions
+	# xFinal = geonames_resultX['timezoneId']
+	# yFinal = geonames_resultY['timezoneId']
 
-rX = requests.get(urlX) ## Make a request
-rY = requests.get(urlY) ## Make a request
-xFinal = rX.json()['timezoneId'] ## return the timezone
-YFinal = rY.json()['timezoneId'] ## return the timezone
+	# ALT METHOD (Doesn't use geonames module)
+	urlX = "http://api.geonames.org/timezoneJSON?formatted=true&lat={}&lng={}&username=demo".format(xLat,xLong)
+	urlY = "http://api.geonames.org/timezoneJSON?formatted=true&lat={}&lng={}&username=demo".format(yLat,yLong)
 
-
-
-
+	rX = requests.get(urlX) ## Make a request
+	rY = requests.get(urlY) ## Make a request
+	xFinal = rX.json()['timezoneId'] ## return the timezone
+	YFinal = rY.json()['timezoneId'] ## return the timezone
