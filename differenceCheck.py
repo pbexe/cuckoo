576830caf14301f85204fed43f137794a6f56338
Zac <allenzj@cardiff.ac.uk>
2017-10-21 16:52:39 +0100

    First full draft of differenceChecker.py

--- differenceCheck.py
+++ differenceCheck.py
@@ -2,17 +2,39 @@
 # flight time
 # time difference
 import requests
+import geonames
 
 # x and y is location name googlemaps
 differenceCheck(x,y):
 	String(x)
 	String(y)
-
+	# concatinate string with google API for Lat/Long request
 	responseX = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+x, safe=' '))
 	responseY = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+y, safe=' '))
 
+	# Parse for Lat/Long
 	respX_json_payload = responseX.json()
 	respY_json_payload = responseY.json()
 
-	x(respX_json_payload['results'][0]['geometry']['location'])
-	x(respY_json_payload['results'][0]['geometry']['location'])
+	# print Lat/Long in dictionaries
+	x = (respX_json_payload['results'][0]['geometry']['location'])
+	y = (respY_json_payload['results'][0]['geometry']['location'])
+
+	# Split Lat/Long from dictionary
+	xLat = x['lat'] 
+	xLong = x['lng']
+
+	yLat = y['lat']
+	yLong = y['lng']
+
+	# Get timezone
+
+	geonames_client = geonames.GeonamesClient('demo')
+	geonames_resultX = geonames_client.find_timezone({xlat, xlong})
+	geonames_resultY = geonames_client.find_timezone({ylat, ylong})
+
+	print(geonames_resultX['timezoneId'])
+	print(geonames_resultY['timezoneId'])
+
+	xFinal = geonames_resultX['timezoneId']
+	yFinal = geonames_resultY['timezoneId']
