ea1236a3a9c13265c49ea8f5699f2859713f2380
Zac <allenzj@cardiff.ac.uk>
2017-10-21 15:02:12 +0100

    Python Psuedo code added to repo

---
+++ PythonPsuedoHackathon.py
@@ -0,0 +1,12 @@
+#Psuedocode for Python/Java
+	#start app and start script 
+	x = Get(systemTime(JourneyStart()));
+	# App sleeps when closed but stores x
+	# App is re-opened by user
+	# App gets n
+	n = Get(systemTime(Finish(Journey Finish())))
+	# Difference in time
+	dif = Compare(x,n)
+	if dif<=0:
+		# Function to adjust 
+		alarmDoctor(int dif)
