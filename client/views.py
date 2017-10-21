a4b3a88d289f48a542f5393485949309ad2d8476
Miles Budden <miles@budden.net>
2017-10-21 17:21:08 +0100

    Added location

--- client/views.py
+++ client/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'base.html')
+
+def api(request):
+    if request.method == 'POST':
+        print(request.POST.get('destination'))
+        print(request.POST.get('lat'))
+        print(request.POST.get('long'))
+    return HttpResponse("OK")
---
+++ flow.md
@@ -0,0 +1,2 @@
+- Get user's location and where they are flying to
+- Pass this to the server
