2a2b75171d98d5d8bb58245dad8cb7d2385b393a
Zac <allenzj@cardiff.ac.uk>
2017-10-21 17:10:28 +0100

    Syntax fixed for differenceChecker.py

--- differenceCheck.py
+++ differenceCheck.py
@@ -5,38 +5,38 @@ import requests
 import geonames
 
 # x and y is location name googlemaps
-differenceCheck(x,y):
-	String(x)
-	String(y)
-	# concatinate string with google API for Lat/Long request
-	responseX = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+x, safe=' '))
-	responseY = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+y, safe=' '))
+differenceCheck(x,y)
+String(x)
+String(y)
+# concatinate string with google API for Lat/Long request
+responseX = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+x, safe=' '))
+responseY = requests.get(urllib.quote('https://maps.googleapis.com/maps/api/geocode/'+y, safe=' '))
 
-	# Parse for Lat/Long
-	respX_json_payload = responseX.json()
-	respY_json_payload = responseY.json()
+# Parse for Lat/Long
+respX_json_payload = responseX.json()
+respY_json_payload = responseY.json()
 
-	# print Lat/Long in dictionaries
-	x = (respX_json_payload['results'][0]['geometry']['location'])
-	y = (respY_json_payload['results'][0]['geometry']['location'])
+# print Lat/Long in dictionaries
+x = (respX_json_payload['results'][0]['geometry']['location'])
+y = (respY_json_payload['results'][0]['geometry']['location'])
 
-	# Split Lat/Long from dictionary
-	xLat = x['lat'] 
-	xLong = x['lng']
+# Split Lat/Long from dictionary
+xLat = x['lat'] 
+xLong = x['lng']
 
-	yLat = y['lat']
-	yLong = y['lng']
+yLat = y['lat']
+yLong = y['lng']
 
-	# Get timezone
+# Get timezone
 
-	geonames_client = geonames.GeonamesClient('demo')
-	geonames_resultX = geonames_client.find_timezone({xlat, xlong})
-	geonames_resultY = geonames_client.find_timezone({ylat, ylong})
+geonames_client = geonames.GeonamesClient('demo')
+geonames_resultX = geonames_client.find_timezone({xlat, xlong})
+geonames_resultY = geonames_client.find_timezone({ylat, ylong})
 
-	# Test prints
-	# print(geonames_resultX['timezoneId'])
-	# print(geonames_resultY['timezoneId'])
+# Test prints
+# print(geonames_resultX['timezoneId'])
+# print(geonames_resultY['timezoneId'])
 
-	# final variables available to other functions
-	xFinal = geonames_resultX['timezoneId']
-	yFinal = geonames_resultY['timezoneId']
+# final variables available to other functions
+xFinal = geonames_resultX['timezoneId']
+yFinal = geonames_resultY['timezoneId']
